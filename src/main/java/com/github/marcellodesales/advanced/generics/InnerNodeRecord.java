226f540731b8e570501f25b1360870181ff5a56b
Marcello DeSales <marcello.desales@gmail.com>
2023-02-08 23:38:44 -0800

    :coffee: just a conversion of generics to records

---
+++ src/main/java/com/github/marcellodesales/advanced/generics/InnerNodeRecord.java
@@ -0,0 +1,29 @@
+package com.github.marcellodesales.advanced.generics;
+
+import java.util.Collections;
+
+public record InnerNodeRecord<T>(T value, TreeNode<T> left,
+                                 TreeNode<T> right) implements TreeNode<T> {
+
+    @Override
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    @Override
+    public TreeNode<T> getRight() {
+        return right;
+    }
+
+    @Override
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        final String bottom = "<- " + left + "  ....  " + right + " ->";
+        return String.join("", Collections.nCopies(bottom.length() / 3, " ")) +
+                "/ InnerNode (" + value + ") \\ \n" + bottom;
+    }
+}
---
+++ src/main/java/com/github/marcellodesales/advanced/generics/LeafNodeRecord.java
@@ -0,0 +1,35 @@
+package com.github.marcellodesales.advanced.generics;
+
+public record LeafNodeRecord<T>(T value) implements TreeNode<T> {
+
+    @Override
+    public T getValue() {
+        return this.value;
+    }
+
+    @Override
+    public TreeNode<T> getLeft() {
+        return null;
+    }
+
+    @Override
+    public TreeNode<T> getRight() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "InnerNode{" +
+                "value=" + value +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        var three = new LeafNodeRecord<Integer>(3);
+        // with type inference.
+        var five = new LeafNodeRecord<>(5);
+        var binTree = new InnerNode<>(10, five, three);
+
+        System.out.println(binTree);
+    }
+}
